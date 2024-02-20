4dd7559f3ce945770402a3063a9b102c287f5a06
EllyIan <ronixeli45@gmail.com>
2024-02-20 00:58:25 -0800

    Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0
 
-SELECT DISTINCT score, id FROM second_table WHERE score = score ORDER BY score DESC;
+SELECT DISTINCT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
