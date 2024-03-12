44ded5a593c5561c88e03652d57a19e3c8454ad9
Ellyian <ronixeli45@gmail.com>
2024-03-12 15:22:32 +0300

    Value of my argument

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
+
+const size = Math.floor(Number(process.argv[2]));
+
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < x; i++) {
-    let line = ' ';
+  for (let i = 0; i < size; i++) {
+    let line = '';
 
-    for (let j = 0; j < x; j++) {
+    for (let j = 0; j < size; j++) {
       line += 'X';
     }
     console.log(line);
   }
 }
+
