1ebb7026312b1df08d59eb92764fd0549123f363
Ellyian <ianmongi45@gmail.com>
2024-03-12 09:47:41 +0000

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 
-const process = require('module');
-
-function add (a , b ) {
+function add (a, b) {
 
     return a + b;
-}
-if (typeof process !== 'undefined') {
-	if (process.argv.length !== 3) {
-	  console.log('Usage: add.js <number1> <number2>');
-	  process.exit(1);
-	}
 }
 
 console.log(add(Number(process.argv[2]) , Number(process.argv[3])));
