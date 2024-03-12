545c4e8d688706c97edfa01b26a76e9db5d0d422
Ellyian <ronixeli45@gmail.com>
2024-03-12 14:52:46 +0300

    fixes

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const x = Math.floor(Number(process.argv[2]));
-if (isNaN (x)) {
-    console.log('Missing size')
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    let line = ' ';
+
+    for (let j = 0; j < x; j++) {
+      line += 'X';
     }
-    else {
-        for (let i = 0; i < x; i++){
-            let line = ' ';
-        
-        for (let j= 0; j < x; j++){
-             line +=   'X';
-        }
-        console.log(line);
-    }
- }
+    console.log(line);
+  }
+}
